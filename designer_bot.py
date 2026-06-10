9561ca8937d5faf98edb2675b94850b7e9536483
art.diz <art.diz@MacBook-Air-artdiz.local>
2026-06-10 16:06:23 +0400

    add start banner

--- designer_bot.py
+++ designer_bot.py
@@ -94,7 +94,11 @@ async def start(update: Update, context) -> None:
         return
     USER_MODE[user_id] = ""
     CONVERSATIONS[user_id] = []
-    await update.message.reply_text("Привет 👋\n\nЧто делаем?", reply_markup=get_main_keyboard())
+    await update.message.reply_photo(
+        photo="https://i.ibb.co/VY7CY8mF/Frame-3.png",
+        caption="Выбери что делаем 👇",
+        reply_markup=get_main_keyboard()
+    )
 
 
 async def register(update: Update, context) -> None:
